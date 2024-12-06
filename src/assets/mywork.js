749521561de658e8b89d30f59bee8e6374f2dc67
Adebayoemma <120459142+Adebayoemma@users.noreply.github.com>
2024-12-06 03:39:31 -0800

    addition of my work page

---
+++ src/assets/mywork.js
@@ -0,0 +1,41 @@
+import Mywork_img from "../compornent/img/01.png";
+// import Mywor1_img from "../compornent/img/Chats.png";
+// import Mywor2_img from "../compornent/img/Discover (overflow behavior).png";
+import Mywor3_img from "../compornent/img/mywork2.png";
+import Mywor4_img from "../compornent/img/mywrk.png";
+// import Mywor5_img from "../compornent/img/Logged out.png";
+
+const mywork_data = [
+  {
+    w_no: 1,
+    w_name: "web design",
+    w_img: Mywork_img,
+  },
+  {
+    w_no: 2,
+    w_name: "web design",
+    w_img: Mywor3_img,
+  },
+  {
+    w_no: 3,
+    w_name: "web design",
+    w_img: Mywork_img,
+  },
+  {
+    w_no: 3,
+    w_name: "web design",
+    w_img: Mywor4_img,
+  },
+  {
+    w_no: 4,
+    w_name: "web design",
+    w_img: Mywor3_img,
+  },
+  {
+    w_no: 5,
+    w_name: "web design",
+    w_img: Mywor4_img,
+  },
+];
+
+export default mywork_data;
